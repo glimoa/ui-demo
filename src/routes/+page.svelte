56d67176bff33fbe1e4c2f05cf925f91fad3de9d
glimoa <glimoa@outlook.com>
2024-06-08 02:55:01 +0900

    add flowbite-svelte and set style css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="ui demo by SvelteKit + tailwind" />
 </svelte:head>
 
 <section>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,11 +1,15 @@
 <script>
-	import { page } from '$app/stores';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
 </script>
 
 <header>
-	<a href="https://github.com/sveltejs/kit">
-		<img  class="object-contain h-16 w-16" src={github} alt="GitHub" />
-	</a>
+	<div class="flex">
+		<a href="https://svelte.jp/">
+			<img  class="object-contain h-16 w-16" src={logo} alt="Svelte" />
+		</a>
+		<a href="https://github.com/sveltejs/kit">
+			<img  class="object-contain h-16 w-16" src={github} alt="GitHub" />
+		</a>
+	</div>
 </header>
